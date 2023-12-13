904c609da15c9caf33fc788fcf492f8a263fa7af
RafisBG <Khottabich@mail.ru>
2023-12-13 15:20:33 +0500

    13.12

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ let sign = ''; //знак операции
 let finish = false;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', 'X', '/'];
+const action = ['-', '+', 'X', '/'];   //массив action
 
 // Экран
 const out = document.querySelector('.calc-screen p')
@@ -39,6 +39,15 @@ document.querySelector('.buttons').onclick = (event) => {
             console.log(a, b, sign)
             out.textContent = a;
         }
+        else if(a!=='' && b!=='' && finish){
+
+        }
+        else{
+            b += key;  
+            out.textContent = b;
+        }
+        console.log(a, b, sign);
+        return;
     }
     // если нажата клавиша + - / *
     if (action.includes(key)){
@@ -46,6 +55,34 @@ document.querySelector('.buttons').onclick = (event) => {
         out.textContent = sign;  // textContent предоставляет доступ к тексту внутри элемента за вычетом всех <тегов>.
         console.log(a, b, sign);
         return;
+    }
+    //нажата =
+    if(key === '='){
+        if(b === '') b = a;
+        switch(sign){
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "X":
+                if(b === '0'){
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a * b;
+                break;
+            case "/":
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.log(a, b, sign);
     }
 }
 
